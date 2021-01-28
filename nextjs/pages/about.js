302d01b006862a7d332c0f1422c4536478064bda
vzivkovic <vzivkovic@me.com>
2021-01-28 12:13:13 +0100

    Initial commit

---
+++ nextjs/pages/about.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Link from 'next/link'
+
+function About() {
+  return (
+    <>
+      <Link href="/">
+        <a>Home</a>
+      </Link>
+      <h1>About</h1>
+    </>
+  )
+}
+
+export default About
